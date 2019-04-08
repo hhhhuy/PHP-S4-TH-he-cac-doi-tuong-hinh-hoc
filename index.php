087501c8febd0bb88db40fa63aa4859c221fed72
hhhuy <hhuuyy95@gmail.com>
2019-04-08 16:22:16 +0700

    he cac doi tuong hinh hoc

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once('Circle.php');
+include_once('Cylinder.php');
+include_once('Rectangle.php');
+include_once('Square.php');
+$circle = new Circle('Circle 01', 6);
+echo 'Circle area: ' . $circle->calculateArea() . '<br />';
+echo 'Circle perimeter: ' . $circle->calculatePerimeter() . '<br />';
+$cylinder = new Cylinder('Cylinder 01', 5, 7);
+echo 'Cylinder area: ' . $cylinder->calculateArea() . '<br />';
+echo 'Cylinder perimeter: ' . $cylinder->calculatePerimeter() . '<br />';
+$rectangle = new Rectangle('Rectangle 01', 2, 6);
+echo 'Rectangle area: ' . $rectangle->calculateArea() . '<br />';
+echo 'Rectangle perimeter: ' . $rectangle->calculatePerimeter() . '<br />';
+$square = new Square('Square 01', 7, 7, 7);
+echo 'Rectangle area: ' . $square->calculateArea() . '<br />';
+echo 'Rectangle perimeter: ' . $square->calculatePerimeter() . '<br />';
